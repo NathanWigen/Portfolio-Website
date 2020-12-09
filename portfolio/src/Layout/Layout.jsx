152b6fc37a0ca97d95746078fb382f74c8315fb0
Nathan Wigen <nathan.s.wigen@hotmail.com>
2020-12-09 13:46:22 -0800

    add Footer to layout

--- portfolio/src/Layout/Layout.jsx
+++ portfolio/src/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 
 export default function Layout() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
