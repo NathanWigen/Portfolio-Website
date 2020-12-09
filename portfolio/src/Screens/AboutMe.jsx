02d91b329fa8081f7643806aa4891296eef6c6e9
Nathan Wigen <nathan.s.wigen@hotmail.com>
2020-12-09 06:18:23 -0800

    import BrowserRouter in index and made Aboutme screen.

---
+++ portfolio/src/Screens/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <div>
+      <div>Hi I'm,</div>
+      <div>Nathan Wigen</div>
+      <div>I am a Front End Developer based in Washington State who enjoys the fast pace of working as a developer.</div>
+      <button>RESUME</button>
+    </div>
+  )
+}
