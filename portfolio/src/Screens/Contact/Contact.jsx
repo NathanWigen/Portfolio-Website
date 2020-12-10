67126390e0dd9e815830d82d0513c27175ba7fa2
Nathan Wigen <nathan.s.wigen@hotmail.com>
2020-12-10 13:15:49 -0800

    Add styling to Contact

--- portfolio/src/Screens/Contact/Contact.jsx
+++ portfolio/src/Screens/Contact/Contact.jsx
@@ -1,40 +1,57 @@
 import React from 'react'
+import './Contact.css'
 
 export default function Contact() {
   return (
     <div>
       <form name="contact" action="/contact" method="post">
         <div>
-          <h3>Contact Me</h3>
-          <input type="hidden" name="form-name" value="contact"/>
-          <label>Name 
-            <input
-              required
-              type="text"
-              name="name"
-              placeholder="Your name"
-            />
-          </label>
-          <label>Email 
-            <input
-              required
-              type="text"
-              name="email"
-              placeholder="Email"
-            />
-          </label>
-          <label>Message
-            <textarea
-              required
-              type="text"
-              name="message"
-              placeholder="Message..."
-              cols="30"
-              rows="10"
-            />
-          </label>
+          <div className="contact-title">
+            <h3>Contact Me</h3>
+          </div>
+          <div className="contact-form">
+            <input type="hidden" name="form-name" value="contact"/>
+            <div className="labels-div">
+              <div className="label-title">
+                <label>Name</label>
+              </div>
+                <input
+                  required
+                  type="text"
+                  name="name"
+                  placeholder="Your name"
+                  className="input"
+                />
+            </div>
+            <div className="labels-div">
+              <div className="label-title">
+                <label>Email</label>
+              </div>
+                <input
+                  required
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  className="input"
+                />
+            </div>
+            <div className="labels-div">
+              <div className="message-title">
+                <label>Message</label>
+              </div>
+                <textarea
+                  required
+                  type="text"
+                  name="message"
+                  placeholder="Message..."
+                  cols="100"
+                  rows="5"
+                  className="text-area"
+                />
+              </div>
+          </div>
         </div>
-        <button type="submit">Send A Message</button>
+        <button type="submit" className="send-button">Send a Message</button>
       </form>
     </div>
   )
