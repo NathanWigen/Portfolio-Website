87d9c996cab90dc2018a844f7ab06398bf096ea0
Nathan Wigen <nathan.s.wigen@hotmail.com>
2020-12-11 07:59:18 -0800

    fixed typos and add tech stack to projects

--- portfolio/src/Screens/AboutMe/AboutMe.jsx
+++ portfolio/src/Screens/AboutMe/AboutMe.jsx
@@ -5,13 +5,13 @@ export default function AboutMe() {
   return (
     <div className="about-me">
       <div className="about-hi">
-        <p>Hi I'm,</p>
+        <p>Hi I'm</p>
         <p>
           NATHAN WIGEN
         </p>
       </div>
       <div className="about-brand">
-        <p>I am a Front End Developer based in Washington State who enjoys the fast pace of working as a developer. Seeking and entry level position to utilize my computer, and service skills. I am self-motivated, quick to learn and a hard-working individual who is energetic and committed to an excellent customer service experience. I have been proven to be flexible, prompt, resourceful, organized and detail-oriented in the workplace. </p>
+        <p>I am a Front End Developer based in Washington State who enjoys the fast pace of working as a developer. Seeking an entry level position to utilize my computer, and service skills. I am self-motivated, quick to learn and a hard-working individual who is energetic and committed to an excellent customer service experience. I have been proven to be flexible, prompt, resourceful, organized, and detail-oriented in the workplace.</p>
       </div>  
       <div className="code">
         <h4>Programming Languages / Frameworks</h4>
@@ -30,6 +30,14 @@ export default function AboutMe() {
         <div>
           <h4>CSS</h4>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png" alt="css-logo" width="50" height="50"/>
+        </div>
+        <div>
+          <h4>Ruby on Rails</h4>
+          <img src="https://www.iglobsyn.com/wp-content/uploads/2019/09/Ruby-on-rails-development.png" alt="css-logo" width="50" height="50"/>
+        </div>
+        <div>
+          <h4>Express / Node.js / MongoDB</h4>
+          <img src="https://miro.medium.com/max/600/1*NFTgwZ_TUmceZnSHCundBw.jpeg" alt="css-logo" width="100" height="70"/>
         </div>
       </div>
       <div className="resume-link">
